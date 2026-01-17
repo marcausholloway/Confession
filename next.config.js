48257c6c4d774eb59e980c15daacf6b566e6347b
ABDULLAH AL NUMAN <111377733+marcausholloway@users.noreply.github.com>
2026-01-17 20:50:32 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/**',
-      },
-    ],
-  },
-  experimental: {
-    serverComponentsExternalPackages: ['cloudinary'],
-  },
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
